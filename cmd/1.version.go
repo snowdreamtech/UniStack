a246ec8d0b5d4709e3f26bbc93db8c75ede56132
snowdream <snowdream@users.noreply.github.com>
2026-07-01 05:53:06 +0800

    feat: port version command

---
+++ cmd/1.version.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2026 SnowdreamTech. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+
+	"github.com/snowdreamtech/unigo/internal/pkg/env"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:     "version",
+	Short:   "Print the version number of " + env.ProjectName,
+	Long:    "Display version information including build details, commit hash, and build time.",
+	Aliases: []string{"v"},
+	Run:     runVersion,
+}
+
+func runVersion(cmd *cobra.Command, args []string) {
+	osArch := runtime.GOOS + "/" + runtime.GOARCH
+	buildVersion := fmt.Sprintf("%s version %s-%s %s\n", env.ProjectName, env.GitTag, env.CommitHash, osArch)
+	copyrightDetail := fmt.Sprintf("%s\n", env.COPYRIGHT)
+	licenseDetail := fmt.Sprintf("License: %s\n", env.LICENSE)
+	authorDetail := fmt.Sprintf("Written by %s", env.Author)
+	buildDetail := fmt.Sprintf("Built at %s", env.BuildTime)
+
+	var builder strings.Builder
+	builder.WriteString("\n")
+	builder.WriteString(buildVersion)
+	builder.WriteString(copyrightDetail)
+	builder.WriteString(licenseDetail)
+	builder.WriteString("\n")
+	builder.WriteString(authorDetail)
+	builder.WriteString("\n")
+	builder.WriteString(buildDetail)
+
+	fmt.Println(builder.String())
+}
