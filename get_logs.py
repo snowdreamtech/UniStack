78bf718d28f427fd77d6164020b30b29d3d1d908
snowdream <snowdream@users.noreply.github.com>
2026-07-10 15:19:24 +0800

    chore: remove temporary debug scripts

--- get_logs.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import zipfile
-import os
-import glob
-
-# Try to download the workflow logs zip
-url = "https://github.com/snowdreamtech/UniStack/actions/runs/29070537998/logs"
-# Unfortunately, GitHub API requires authentication to download logs even for public repos using the /logs endpoint.
-# But we can try!
